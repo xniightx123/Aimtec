6cca87e8bdc61192a8c5eb7959fdd371206f518d
blacky <blacky@kuroneko.me>
2017-07-23 00:44:52 +0200

    BlackFeeder: remove useless functions

--- BlackFeeder/Utility.cs
+++ BlackFeeder/Utility.cs
@@ -14,22 +14,5 @@ namespace BlackFeeder
             FLW = 3,
             RND = 4
         }
-
-        public enum FType
-        {
-            AllyF,
-            EnemyF
-        }
-
-        public static SpellSlot GetSpellSlot(Obj_AI_Hero unit, string name)
-        {
-            foreach (
-                var spell in
-                unit.SpellBook.Spells.Where(x => string.Equals(x.Name, name, StringComparison.CurrentCultureIgnoreCase))
-            )
-                return spell.Slot;
-
-            return SpellSlot.Unknown;
-        }
     }
 }
